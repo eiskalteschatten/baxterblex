cd7894abc02a6d02211f8a958a308a4f086a2c09
Alex Seifert <alex@alexseifert.com>
2022-05-23 11:19:23 +0200

    Initial Commit

---
+++ Shared/BaxterblexApp.swift
@@ -0,0 +1,20 @@
+//
+//  BaxterblexApp.swift
+//  Shared
+//
+//  Created by Alex Seifert on 23.05.22.
+//
+
+import SwiftUI
+
+@main
+struct BaxterblexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
