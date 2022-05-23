eaddd0e4fb8d227d534b2bae8f4650a38d1d6f83
Alex Seifert <alex@alexseifert.com>
2022-05-23 16:58:07 +0200

    Use the standard SwiftUI navigation view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,36 +9,82 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @State private var showEditSheet = false
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Game.name, ascending: true)],
+        animation: .default
+    )
+    private var games: FetchedResults<Game>
 
     var body: some View {
-        // TODO: Restore the game view if there is a selected game from the last session
-        
-        ChooseGameView()
-        
-//        NavigationView {
-//            List {
-//                ForEach(items) { item in
-//                    NavigationLink {
-//                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-//                    } label: {
-//                        Text(item.timestamp!, formatter: itemFormatter)
-//                    }
-//                }
-//                .onDelete(perform: deleteItems)
-//            }
-//            .toolbar {
-//#if os(iOS)
-//                ToolbarItem(placement: .navigationBarTrailing) {
-//                    EditButton()
-//                }
-//#endif
-//                ToolbarItem {
-//                    Button(action: addItem) {
-//                        Label("Add Item", systemImage: "plus")
-//                    }
-//                }
-//            }
-//        }
+        NavigationView {
+            List {
+                Section("Games") {
+                    ForEach(games.filter { !$0.archived }) { game in
+                        NavigationLink {
+                            // TODO: add game view
+                            Text(game.name ?? DEFAULT_GAME_NAME)
+                        } label: {
+                            // TODO: add icon or color or something else
+                            Text(game.name ?? DEFAULT_GAME_NAME)
+                        }
+                    }
+                    .onDelete(perform: deleteGames)
+                }
+                
+                Section("Archived Games") {
+                    ForEach(games.filter { $0.archived }) { game in
+                        NavigationLink {
+                            // TODO: add game view
+                            Text(game.name ?? DEFAULT_GAME_NAME)
+                        } label: {
+                            // TODO: add icon or color or something else
+                            Text(game.name ?? DEFAULT_GAME_NAME)
+                        }
+                    }
+                    .onDelete(perform: deleteGames)
+                }
+            }
+            .toolbar {
+                #if os(iOS)
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                #endif
+                ToolbarItem {
+                    Button(action: { showEditSheet.toggle() }) {
+                        Label("Create a New Game", systemImage: "plus")
+                    }
+                }
+            }
+            #if os(iOS)
+            .navigationBarTitle("Baxterblex")
+            #endif
+            
+            // TODO: show monchrome icon
+            Text("TODO: show monochrome icon")
+        }
+        .sheet(isPresented: $showEditSheet) {
+            EditGameSheet()
+        }
+    }
+    
+    private func deleteGames(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { games[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
     }
 }
 
