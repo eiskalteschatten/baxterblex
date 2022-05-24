989ea965f6bf01b9bfd22d00e0504ab8ebdf7035
Alex Seifert <alex@alexseifert.com>
2022-05-24 14:28:42 +0200

    Start trying to figure out a spot to put the save and cancel buttons

--- Shared/Characters/EditCharacter/EditCharacterView.swift
+++ Shared/Characters/EditCharacter/EditCharacterView.swift
@@ -18,10 +18,45 @@ struct EditCharacterView: View {
     }
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Name", text: $editCharacterModel.name)
+        VStack {
+            #if os(iOS)
+            CancelSaveButtons()
+            #endif
+            
+            Form {
+                Section {
+                    TextField("Name", text: $editCharacterModel.name)
+                }
+                
+                #if os(macOS)
+                CancelSaveButtons()
+                #endif
             }
+        }
+    }
+}
+
+fileprivate struct CancelSaveButtons: View {
+    @EnvironmentObject var gameStore: GameStore
+    
+    var body: some View {
+        HStack {
+            Button("Cancel") {
+                gameStore.createCharacter = false
+                gameStore.selectedCharacter = nil
+            }
+            .keyboardShortcut(.cancelAction)
+            #if os(iOS)
+            .frame(maxWidth: .infinity)
+            #endif
+            
+            Button("Save") {
+                
+            }
+            .keyboardShortcut(.defaultAction)
+            #if os(iOS)
+            .frame(maxWidth: .infinity)
+            #endif
         }
     }
 }
