766d6413f9cc614452f3b968e27919c36d5da6f7
Alex Seifert <alex@alexseifert.com>
2022-05-24 16:11:38 +0200

    Add a temporary fix for views making the macOS window not resizable

--- Shared/Accounting/AccountingView.swift
+++ Shared/Accounting/AccountingView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct AccountingView: View {
     var body: some View {
         Text("Accounting")
+            #if os(macOS)
+            // Temp fix until this view is done
+            .frame(minWidth: 500, maxWidth: .infinity, minHeight: 400, maxHeight: .infinity)
+            #endif
     }
 }
 
