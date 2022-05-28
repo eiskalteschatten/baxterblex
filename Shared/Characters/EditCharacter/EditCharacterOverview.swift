10703cc9b98f15f43a35c9f7ad435c70909e12df
Alex Seifert <alex@alexseifert.com>
2022-05-28 12:06:12 +0200

    Add the rest of the rich text editors for characters

--- Shared/Characters/EditCharacter/EditCharacterOverview.swift
+++ Shared/Characters/EditCharacter/EditCharacterOverview.swift
@@ -11,11 +11,42 @@ struct EditCharacterOverview: View {
     @ObservedObject var editCharacterModel: EditCharacterModel
     
     var body: some View {
-        VStack {
-            TextField("Name", text: $editCharacterModel.name)
-            
-            Text("Biography")
-            RichTextEditor(text: $editCharacterModel.biography)
+        let textEditorHeight: CGFloat = 200
+        
+        ScrollView {
+            VStack(alignment: .leading, spacing: 25) {
+                TextField("Name", text: $editCharacterModel.name)
+                
+                VStack(alignment: .leading) {
+                    Text("Biography")
+                    RichTextEditor(text: $editCharacterModel.biography)
+                        .frame(height: textEditorHeight)
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("Family & Friends")
+                    RichTextEditor(text: $editCharacterModel.familyFriends)
+                        .frame(height: textEditorHeight)
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("Hobbies")
+                    RichTextEditor(text: $editCharacterModel.hobbies)
+                        .frame(height: textEditorHeight)
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("Occupation")
+                    RichTextEditor(text: $editCharacterModel.occupation)
+                        .frame(height: textEditorHeight)
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("Notes")
+                    RichTextEditor(text: $editCharacterModel.notes)
+                        .frame(height: textEditorHeight)
+                }
+            }
         }
     }
 }
