a11c0f3b66e6e09e076357a3f4ca51f12037a54d
Alex Seifert <alex@alexseifert.com>
2022-06-01 13:16:47 +0200

    Fetch all types belonging to a character

--- Shared/Characters/EditCharacter/EditCharacterAttributes/CharacterAttributeEditorSheet.swift
+++ Shared/Characters/EditCharacter/EditCharacterAttributes/CharacterAttributeEditorSheet.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct CharacterAttributeEditorSheet: View {
+    @EnvironmentObject private var gameStore: GameStore
+    
     var body: some View {
-        #if os(macOS)
-        MacCharacterAttributeEditor()
-        #else
-        // TODO
-        #endif
+        if let character = gameStore.selectedCharacter {
+            #if os(macOS)
+            MacCharacterAttributeEditor(character: character)
+            #else
+            // TODO
+            #endif
+        }
+        else {
+            Text("No character selected")
+        }
     }
 }
 
--- Shared/Characters/EditCharacter/EditCharacterAttributes/MacCharacterAttributeEditor.swift
+++ Shared/Characters/EditCharacter/EditCharacterAttributes/MacCharacterAttributeEditor.swift
@@ -10,10 +10,30 @@ import SwiftUI
 struct MacCharacterAttributeEditor: View {
     @Environment(\.dismiss) var dismiss
     
+    var character: Character
+    
+    @State private var selectedType: CharacterAttributeType?
+    @State private var selectedCategory: CharacterAttributeCategory?
+    @State private var selectedAttribute: CharacterAttribute?
+    
+    @FetchRequest private var types: FetchedResults<CharacterAttributeType>
+    
+    init(character: Character) {
+        self.character = character
+        
+        self._types = FetchRequest<CharacterAttributeType>(
+            sortDescriptors: [NSSortDescriptor(keyPath: \CharacterAttributeType.name, ascending: true)],
+            predicate: NSPredicate(format: "character == %@", character),
+            animation: .default
+        )
+    }
+    
     var body: some View {
         VStack {
             HSplitView {
-                Text("Types")
+                List(types, id: \.self, selection: $selectedType) { type in
+                    Text(type.name ?? "")
+                }
                 
                 Text("Categories")
                 
@@ -38,6 +58,9 @@ struct MacCharacterAttributeEditor: View {
 
 struct MacCharacterAttributeEditor_Previews: PreviewProvider {
     static var previews: some View {
-        MacCharacterAttributeEditor()
+        let context = PersistenceController.preview.container.viewContext
+        let character = context.registeredObjects.first(where: { $0 is Character }) as! Character
+        
+        MacCharacterAttributeEditor(character: character)
     }
 }
